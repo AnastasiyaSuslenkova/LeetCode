680029028302ee5d207b09b38e820f61bc106931
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-07-03 23:32:41 +0700

    Add files via upload

---
+++ Valid Anagram.py
@@ -0,0 +1,34 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+#
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+#
+#
+# Constraints:
+#
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+
+
+def is_anagram(s: str, t: str) -> bool:
+    for letter in set(s + t):
+        if s.count(letter) != t.count(letter):
+            return False
+    return True
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tests = [{'s': 'anagram', 't': 'nagaram', 'expected': True},
+             {'s': 'rat', 't': 'car', 'expected': False}]
+    for test in tests:
+        print(test, is_anagram(test['s'], test['t']))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
