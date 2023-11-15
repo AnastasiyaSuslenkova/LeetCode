6c83ba4d9c54ffdc3421f9eddd78e777696c8e52
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-11-15 18:57:21 +0700

    Update string_to_integer.py (pylint)

--- string_to_integer.py
+++ string_to_integer.py
@@ -1,24 +1,29 @@
-# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer
+# Implement the myAtoi(string s) function,
+# which converts a string to a 32-bit signed integer
 # (similar to C/C++'s atoi function).
 #
 # The algorithm for myAtoi(string s) is as follows:
 #
 # 1. Read in and ignore any leading whitespace.
-# 2. Check if the next character (if not already at the end of the string) is '-' or '+'.
-# Read this character in if it is either. This determines if the final result is negative or positive respectively.
+# 2. Check if the next character (if not already at the end of the string)
+# is '-' or '+'. Read this character in if it is either.
+# This determines if the final result is negative or positive respectively.
 # Assume the result is positive if neither is present.
-# 3. Read in next the characters until the next non-digit character or the end of the input is reached.
-# The rest of the string is ignored.
+# 3. Read in next the characters until the next non-digit character
+# or the end of the input is reached. The rest of the string is ignored.
 # 4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32).
-# If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
-# 5. If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], then clamp the integer
-# so that it remains in the range. Specifically, integers less than -2^31 should be clamped to -2^31,
+# If no digits were read, then the integer is 0.
+# Change the sign as necessary (from step 2).
+# 5. If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1],
+# then clamp the integer so that it remains in the range.
+# Specifically, integers less than -2^31 should be clamped to -2^31,
 # and integers greater than 2^31 - 1 should be clamped to 2^31 - 1.
 # 6. Return the integer as the final result.
 #
 # Note:
 #     Only the space character ' ' is considered a whitespace character.
-#     Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+#     Do not ignore any characters other than the leading whitespace
+#     or the rest of the string after the digits.
 #
 #
 #
@@ -26,7 +31,8 @@
 #
 # Input: s = "42"
 # Output: 42
-# Explanation: The underlined characters are what is read in, the caret is the current reader position.
+# Explanation: The underlined characters are what is read in,
+# the caret is the current reader position.
 # Step 1: "42" (no characters read because there is no leading whitespace)
 #          ^
 # Step 2: "42" (no characters read because there is neither a '-' nor '+')
@@ -57,11 +63,14 @@
 # Input: s = "4193 with words"
 # Output: 4193
 # Explanation:
-# Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+# Step 1: "4193 with words" (no characters read because
+# there is no leading whitespace)
 #          ^
-# Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+# Step 2: "4193 with words" (no characters read because
+# there is neither a '-' nor '+')
 #          ^
-# Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+# Step 3: "4193 with words" ("4193" is read in; reading stops because
+# the next character is a non-digit)
 #              ^
 # The parsed integer is 4193.
 # Since 4193 is in the range [-2^31, 2^31 - 1], the final result is 4193.
@@ -73,7 +82,6 @@ import re
 
 def my_atoi(s: str) -> int:
     sgn = 1
-    t = list(takewhile(lambda x: ' ' == x, s))
     s = s[len(list(takewhile(lambda x: x == ' ', s))):]
     if len(s) == 0:
         return 0
