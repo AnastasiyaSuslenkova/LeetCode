0578d7c40943a5995876acf7338ecce7ad3aac21
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-11-15 15:14:20 +0700

    Update longest_consequtive_sequence.py pylint)

--- longest_consequtive_sequence.py
+++ longest_consequtive_sequence.py
@@ -1,59 +1,39 @@
-# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-# You must write an algorithm that runs in O(n) time.
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
 #
 # Example 1:
-# Input: nums = [100,4,200,1,3,2]
-# Output: 4
-# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
 #
 # Example 2:
-# Input: nums = [0,3,7,2,5,8,4,6,0,1]
-# Output: 9
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+#
 #
 # Constraints:
-#     0 <= nums.length <= 10^5
-#     -10^9 <= nums[i] <= 10^9
-from collections import defaultdict
+#
+#     1 <= strs.length <= 200
+#     0 <= strs[i].length <= 200
+#     strs[i] consists of only lowercase English letters.
 
 
-def longest_consecutive(nums: list[int]) -> int:
-    if not nums:
-        return 0
-    dict_key_max_value_min = defaultdict(bool)
-    dict_key_min_value_max = defaultdict(bool)
-    ns = set(nums)
-    for n in ns:
-        if type(dict_key_min_value_max[n + 1]) == int:
-            if dict_key_max_value_min[n - 1]:
-                dict_key_max_value_min[dict_key_min_value_max[n + 1]] = dict_key_max_value_min[n - 1]
-                dict_key_min_value_max[dict_key_max_value_min[n - 1]] = dict_key_min_value_max[n + 1]
-                dict_key_min_value_max[n + 1] = False
-                dict_key_max_value_min[n - 1] = False
-            else:
-                this_max = dict_key_min_value_max[n + 1]
-                dict_key_min_value_max[n] = this_max
-                dict_key_max_value_min[this_max] = n
-                dict_key_min_value_max[n + 1] = False
-        elif type(dict_key_max_value_min[n - 1]) == int:
-            this_min = dict_key_max_value_min[n - 1]
-            dict_key_max_value_min[n] = this_min
-            dict_key_min_value_max[this_min] = n
-            dict_key_max_value_min[n - 1] = False
-        else:
-            dict_key_min_value_max[n] = n
-            dict_key_max_value_min[n] = n
-    res = 0
-    for k, v in dict_key_min_value_max.items():
-        if (type(v) == int) and ((v - k) > res):
-            res = v - k
-    return res + 1
+def longest_common_prefix(strs: list[str]) -> str:
+    for i, s in enumerate(strs[0]):
+        for string in strs[1:]:
+            try:
+                if string[i] != s:
+                    return string[:i]
+            except IndexError:
+                return string[:i]
+    return strs[0]
 
 
 if __name__ == '__main__':
-    tests = [{'inp': [100, 4, 200, 1, 3, 2], 'exp': 4},
-             {'inp': [0, 3, 7, 2, 5, 8, 4, 6, 0, 1], 'exp': 9}]
+    tests = [{'strs': ["flower", "flow", "flight"], 'expected': 'fl'},
+             {'strs': ["dog", "racecar", "car"], 'expected': ''},
+             {'strs': [''], 'expected': ''},
+             {'strs': ['a'], 'expected': 'a'},
+             {'strs': ['ab', 'a'], 'expected': 'a'}]
     for test in tests:
-        print(test, longest_consecutive(test['inp']))
-
-# выглядит страшно, но вроде как должно за O(n) работать (требование есть такое),
-# если посмотреть значение по ключу у defaultdict - O(1)
+        print(test, longest_common_prefix(test['strs']))
