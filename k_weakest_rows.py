44eddc10199c63e976f15aa47a4864a008f8a138
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-11-15 14:43:13 +0700

    Update k_weakest_rows.py (pylint)

--- k_weakest_rows.py
+++ k_weakest_rows.py
@@ -1,4 +1,5 @@
-# You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians).
+# You are given an m x n binary matrix mat of 1's (representing soldiers)
+# and 0's (representing civilians).
 # The soldiers are positioned in front of the civilians.
 # That is, all the 1's will appear to the left of all the 0's in each row.
 
@@ -7,7 +8,8 @@
 # The number of soldiers in row i is less than the number of soldiers in row j.
 # Both rows have the same number of soldiers and i < j.
 
-# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+# Return the indices of the k weakest rows in the matrix ordered
+# from weakest to strongest.
 
 # Examples:
 
@@ -39,7 +41,18 @@ def k_weakest_rows(mat: list[list[int]], k: int) -> list[int]:
 
 
 if __name__ == '__main__':
-    tests = [{'mat': [[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]], 'k': 3, 'expected_result': [2,0,3]},
-             {'mat': [[1,0,0,0],[1,1,1,1],[1,0,0,0],[1,0,0,0]], 'k': 2, 'expected_result': [0,2]}]
+    tests = [
+        {
+            'mat': [[1, 1, 0, 0, 0], [1, 1, 1, 1, 0], [1, 0, 0, 0, 0],
+                    [1, 1, 0, 0, 0], [1, 1, 1, 1, 1]],
+            'k': 3,
+            'expected_result': [2, 0, 3]
+        },
+        {
+            'mat': [[1, 0, 0, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0]],
+            'k': 2,
+            'expected_result': [0, 2]
+        }
+    ]
     for test in tests:
         print(test, k_weakest_rows(test['mat'], test['k']))
