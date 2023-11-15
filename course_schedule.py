5f13e1100f1c6131c49a990f16c4327529da2d3f
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-11-15 13:35:08 +0700

    Update course_schedule.py

--- course_schedule.py
+++ course_schedule.py
@@ -1,6 +1,7 @@
-# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
-# You are given an array prerequisites where prerequisites[i] = [a_i, b_i] indicates that you must take
-# course b_i first if you want to take course a_i.
+# There are a total of numCourses courses you have to take,
+# labeled from 0 to numCourses - 1.
+# You are given an array prerequisites where prerequisites[i] = [a_i, b_i]
+# indicates that you must take course b_i first if you want to take course a_i.
 # For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 # Return true if you can finish all courses. Otherwise, return false.
 #
@@ -14,7 +15,8 @@
 # Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
 # Output: false
 # Explanation: There are a total of 2 courses to take.
-# To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+# To take course 1 you should have finished course 0, and to take course 0
+# you should also have finished course 1. So it is impossible.
 #
 #
 #
@@ -30,7 +32,7 @@
 import numpy as np
 
 
-def can_finish(numCourses: int, prerequisites: list[list[int]]) -> bool:
+def can_finish(num_courses: int, prerequisites: list[list[int]]) -> bool:
     if not prerequisites:
         return True
     viewed = []
@@ -53,8 +55,11 @@ def can_finish(numCourses: int, prerequisites: list[list[int]]) -> bool:
     return True
 
 
-tests = [{'numCourses': 2, 'prerequisites': [[1, 0]], 'expected': True},
-         {'numCourses': 2, 'prerequisites': [[1, 0], [0, 1]], 'expected': False},
-         {'numCourses': 4, 'prerequisites': [[2, 0], [1, 0], [3, 1], [3, 2], [1, 3]], 'expected': False}]
+tests = [
+    {'numCourses': 2, 'prerequisites': [[1, 0]], 'expected': True},
+    {'numCourses': 2, 'prerequisites': [[1, 0], [0, 1]], 'expected': False},
+    {'numCourses': 4, 'prerequisites': [[2, 0], [1, 0], [3, 1], [3, 2], [1, 3]],
+     'expected': False}
+]
 for test in tests:
     print(test, can_finish(test['numCourses'], test['prerequisites']))
