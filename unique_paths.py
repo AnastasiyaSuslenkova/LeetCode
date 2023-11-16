3c9efb9e2fc313a4a69ca37b48450995572164ee
AnastasiyaSuslenkova <100143305+AnastasiyaSuslenkova@users.noreply.github.com>
2023-11-16 10:43:17 +0700

    Update unique_paths.py (pylint)

--- unique_paths.py
+++ unique_paths.py
@@ -1,9 +1,11 @@
-# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+# There is a robot on an m x n grid. The robot is initially located
+# at the top-left corner (i.e., grid[0][0]).
 # The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
 # The robot can only move either down or right at any point in time.
-# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the
-# bottom-right corner.
-# The test cases are generated so that the answer will be less than or equal to 2 * 10^9.
+# Given the two integers m and n, return the number of possible unique paths
+# that the robot can take to reach the bottom-right corner.
+# The test cases are generated so that the answer will be less than
+# or equal to 2 * 10^9.
 #
 # Example 1:
 # Input: m = 3, n = 7
@@ -12,7 +14,8 @@
 # Example 2:
 # Input: m = 3, n = 2
 # Output: 3
-# Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# Explanation: From the top-left corner, there are a total of 3 ways
+# to reach the bottom-right corner:
 # 1. Right -> Down -> Down
 # 2. Down -> Down -> Right
 # 3. Down -> Right -> Down
@@ -36,4 +39,3 @@ if __name__ == '__main__':
              {'m': 23, 'n': 12, 'expected':  193536720}]
     for test in tests:
         print(test, unique_paths(test['m'], test['n']))
-
